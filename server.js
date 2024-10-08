441e8883ded8121ed21dc88705ae2315f654d100
ew42 <evan@craigwall.com>
2024-10-08 17:00:52 -0400

    Unfinished Implementation

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 
+const app =  express();
+
 // Update these paths to point to your certificate files
 const certPath = '/etc/letsencrypt/live/ew42.com/fullchain.pem';
 const keyPath = '/etc/letsencrypt/live/ew42.com/privkey.pem';
@@ -25,60 +27,67 @@ function log(message) {
   logStream.write(logMessage);
 }
 
-const requestHandler = (req, res) => {
+app.get('/', (req, res) => {
   const clientIp = req.socket.remoteAddress;
-  const userAgent = req.headers['user-agent'];
-  log(`HTTPS Request from ${clientIp} - ${userAgent} for ${req.url}`);
-  
-  //What is it requesting? What kind of file is that? Send it.
+  const userAgent = req.headers['user-agnet'];
+  log('Https request from ${clientIp} - ${userAgent} for ${req.url}');
+  res.send('Hello World!');
+}
 
-  let filePath = (__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
-
-  let extname = path.extname(filePath);
-
-  let contentType = 'text/html';
-  switch (extname) {
-    case '.js':
-      contentType = 'text/javascript';
-      break
-    case '.css':
-      contentType = 'text/css';
-      break
-    case '.json':
-      contentType = 'applications/json';
-      break
-    case '.png':
-      contentType = 'image/png';
-      break
-    case '.jpg':
-      contentType = 'image/jpg';
-  }
-
-  fs.readFile(filePath, (error, content) => {
-      if (error) {
-        if (error.code === 'ENOENT') {
-          // File not found
-          fs.readFile(path.join(__dirname, 'public', '404.html'), (error, content) => {
-            res.writeHead(404, { 'Content-Type': 'text/html' });
-            res.end(content, 'utf-8');
-          });
-        }
-        else {
-          // Server error
-          res.writeHead(500);
-          res.end(`Server Error: ${error.code}`);
-        }
-      }
-        else {
-        // Success
-        res.writeHead(200, { 'Content-Type': contentType });
-        res.end(content, 'utf-8');
-      }
-    });
-};
+// const requestHandler = (req, res) => {
+//   const clientIp = req.socket.remoteAddress;
+//   const userAgent = req.headers['user-agent'];
+//   log(`HTTPS Request from ${clientIp} - ${userAgent} for ${req.url}`);
+//
+//   //What is it requesting? What kind of file is that? Send it.
+//
+//   let filePath = (__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
+//
+//   let extname = path.extname(filePath);
+//
+//   let contentType = 'text/html';
+//   switch (extname) {
+//     case '.js':
+//       contentType = 'text/javascript';
+//       break
+//     case '.css':
+//       contentType = 'text/css';
+//       break
+//     case '.json':
+//       contentType = 'applications/json';
+//       break
+//     case '.png':
+//       contentType = 'image/png';
+//       break
+//     case '.jpg':
+//       contentType = 'image/jpg';
+//   }
+//
+//   fs.readFile(filePath, (error, content) => {
+//       if (error) {
+//         if (error.code === 'ENOENT') {
+//           // File not found
+//           fs.readFile(path.join(__dirname, 'public', '404.html'), (error, content) => {
+//             res.writeHead(404, { 'Content-Type': 'text/html' });
+//             res.end(content, 'utf-8');
+//           });
+//         }
+//         else {
+//           // Server error
+//           res.writeHead(500);
+//           res.end(`Server Error: ${error.code}`);
+//         }
+//       }
+//         else {
+//         // Success
+//         res.writeHead(200, { 'Content-Type': contentType });
+//         res.end(content, 'utf-8');
+//       }
+//     });
+// };
 
 // Create HTTPS server
-const httpsServer = https.createServer(options, requestHandler);
+const httpsServer = https.createServer(options, app);
 httpsServer.listen(httpsPort, () => {
   log(`HTTPS Server running on port ${httpsPort}`);
 });
